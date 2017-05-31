de8308afe37675cf4ca4ec268bb3defb1b57a01e
lohit8846 <lohit8846@gmail.com>
2017-05-31 13:01:58 -0700

    Removed DanglingPage array in pagerank

--- algorithms/PageRank.py
+++ algorithms/PageRank.py
@@ -7,48 +7,54 @@ class allPages:
    def __init__(self):
       self.inLinks = {}
       self.numberOfOutLinks = {}
-      self.danglingPages = []
-      
+
    def addPage(self, pageLink, inLinks, numberOfOutLinks):
       self.inLinks[pageLink] = inLinks
       self.numberOfOutLinks[pageLink] = numberOfOutLinks;
-      if (numberOfOutLinks == 0):
-         self.danglingPages.append(pageLink)
-         
-         
+
+
 
 def pagerank(pages, dampening = .85, epsilon = .000001):
 
    links = pages.inLinks.keys()
    n = len(links)
-   
+
    p = numpy.matrix(numpy.ones((n,1)))/n
-   
+
    links = pages.inLinks.keys()
-   
+
    converganceChange = 0
-   
+   numIterations = 0
+
    while converganceChange > e:
-      
+
       m = numpy.matrix(numpy.zeros((n,1)))
-      danglingProduct = sum([p[i] for i in g.dangling_pages.keys()])
+
+      danglingProduct = 0
       for i, link in enumerate(links):
+         if pages.number_out_links[link] == 0:
+            danglingProduct += p[i]
+
+      for i, link in enumerate(links):
+
          linkSum = 0
          for j, inlink in enumerate(pages.inLinks[link]):
             linkSum += p[j]/g.number_out_links[inLink]
+
          sA = dampening * linkSum
          sD = (dampening * danglingProduct) / n
          tE = (1 - dampening) / n
          m[i] = sA + sD + tE
-         
+
       oldP = p
       p = m / numpy.sum(m)
       absChange = numpy.abs(oldP-p)
       converganceChange = numpy.sum(absChange)
-      
+      numIterations += 1
+
    return p
-      
-         
-   
-   
+
+
+
+
 
